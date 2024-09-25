4c5b93e19730516884af0ab4d6b7a6da8a48b42e
Minji Kim <qwe38280@gmail.com>
2024-09-25 11:34:01 +0900

    Change Customer field first name to given name

--- store/models.py
+++ store/models.py
@@ -38,7 +38,7 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold'),
         (MEMBERSHIP_PLATINUM, 'Platinum'),
     ]
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
